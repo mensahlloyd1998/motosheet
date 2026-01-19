7bbedb09bddee5aa19940a040a9e2d85bfeafe35
mensahlloyd1998 <lloydmensah123@gmail.com>
2026-01-19 02:25:21 +0000

    hide some table columns on mobile screens

--- resources/views/luno/cars/index.blade.php
+++ resources/views/luno/cars/index.blade.php
@@ -93,8 +93,8 @@
                     <thead>
                         <tr class="py-3">
                             <th class="fw-light text-dark">vehicle</th>
-                            <th class="fw-light text-dark">page status</th>
-                            <th class="fw-light text-dark">price</th>
+                            <th class="fw-light text-dark d-none d-table-cell">page status</th>
+                            <th class="fw-light text-dark d-none d-table-cell">price</th>
                             <th class="fw-light text-dark">metrics</th>
                             <th class="fw-light text-dark">actions</th>
                         </tr>
@@ -128,11 +128,11 @@
                                     </div>
                                 </div>
                             </td>
-                            <td>
+                            <td class="d-none d-table-cell">
                                 <span class="text-capitalize">{{ $data->status}}</span><br>
                                 <!-- <small class="text-muted"></small> -->
                             </td>
-                            <td>
+                            <td class="d-none d-table-cell">
                                 <span>GHS {{ $data->price}}</span><br>
                                 <!-- <small class="text-muted">Negotiable</small> -->
                             </td>
