4ac2ccc57a16ff40aff150503cf9e076b5c981e8
mensahlloyd1998 <lloydmensah123@gmail.com>
2026-02-21 09:15:13 +0000

    add errors messages to create page

--- resources/views/luno/cars/create.blade.php
+++ resources/views/luno/cars/create.blade.php
@@ -25,6 +25,38 @@
 
 
 @section('content')
+
+    @if(auth()->user()->poster_contact_1 == null)
+        <div class="row">
+            <div class="col-12">
+                <div class="alert alert-warning mb-2">
+                    <span class="cstm-fs-14">Provide contact details to be included on posters/labels</span>
+                </div>
+
+            </div>
+        </div>
+    @endif
+
+    @if(session('success'))
+        <div class="row">
+            <div class="col-12">
+                <div class="alert alert-success mb-2">
+                    <span class="cstm-fs-14">{{  session('success') }}</span>
+                </div>
+
+            </div>
+        </div>
+    @endif
+    @if(session('error'))
+        <div class="row">
+            <div class="col-12">
+                <div class="alert alert-danger mb-2">
+                    <span class="cstm-fs-14">{{  session('error') }}</span>
+                </div>
+
+            </div>
+        </div>
+    @endif
     <div class="row g-3">
         <div class="col-12">
             <div class="card">
